22b6ff426f7a02a82a7aa66a933dab36368735ef
Jessica <jessiberteloni@gmail.com>
2025-10-31 18:02:43 -0300

    Apagando atividades duplicadas

--- Atividade01.js
+++
@@ -1,8 +0,0 @@
-const leia = require('readline-sync');
-
-let salario = leia.questionFloat("Digite o salario: R$");
-let abono = leia.questionFloat("Digite o Abono: R$");
-
-novoSalario = salario + abono;
-
-console.log("Novo Salario: R$" + novoSalario.toFixed(2));
