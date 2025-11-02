f55a937285ffe45796c862c5efafb6e41d9e929c
Jessica <jessiberteloni@gmail.com>
2025-11-01 23:17:26 -0300

    Removida pasta aula01

---
+++ lacoRepeticao/ExercicioFor01.js
@@ -0,0 +1,7 @@
+const leia = require("readline-sync")
+
+let n1, n2
+
+
+n1 = leia.questionInt("Digite o primeiro número do intervalo: 10")
+n2 = leia.questionInt("Digite o último número do intervalo: 100")
